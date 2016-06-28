355e9112e5b78be3d0489da778821ba924d3ae37
vogtn <vogtn@uw.edu>
2016-06-28 13:41:02 -0700

    uploading project code

---
+++ js/index.js
@@ -0,0 +1,43 @@
+/*var number = []; 
+$(function () {
+    $('#zero').on('click', function () {
+        var text = $('#display');
+        text.val(text.val() + "hello");    
+    });
+      $('#AC').on('click', function () {
+        $('#display').empty();   
+    });
+});*/
+var ans = "";
+var clear = false;
+var calc = "";
+$(document).ready(function() {
+  $("button").click(function() {
+    var text = $(this).attr("value");
+     if(parseInt(text, 10) == text || text === "." || text === "/" || text === "*" || text === "-" || text === "+" || text === "%") {
+      if(clear === false) {
+        calc += text;
+        $(".textbox").val(calc);
+      } else {
+        calc = text;
+        $(".textbox").val(calc);
+        clear = false;
+      }
+      
+    } if(text === "AC") {
+      calc = "";
+      $(".textbox").val("");
+    } else if(text === "CE") {
+      calc = calc.slice(0, -1);
+      $(".textbox").val(calc);
+    } else if(text === "=") {
+      ans = eval(calc);
+      $(".textbox").val(ans);
+      clear = true;
+    }else if(text === "And") { 
+      calc = $(".textbox").val(); 
+      $(".textbox").val(calc); 
+      clear=false;
+    }
+  });
+});
